d59958934eacf04f875149df82f02d149c6a81ee
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-05-13 20:53:42 +0200

    added brain killing stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 void konsolenAusgabe()
 {
@@ -23,16 +24,49 @@ void begruessen(){
 
     std::string name;
 
-    std::cin >> name;
+    std::getline(std::cin, name);
 
     std::cout << "Hallo " << name << ", schoen dich kennen zu lernen!" << std::endl;
 
+}
+
+void maxMin(){
+    std::cout << std::max(5,10) << std::endl;
+    std::cout << std::min(5,10) << std::endl;
+}
+
+void quadrieren(int pow){
+    int x;
+    std::cin >> x;
+    std::cout << std::pow(x,pow) << std::endl;
+}
+
+bool isMe(){
+    std::cout << "Gib dein Name ein" << std::endl;
+    std::string name;
+    std::getline(std::cin,name);
+    return  name == "Gabriel Nadolny";
+}
+
+void loops(){
+    std::string words[] = {"Hallo","Welt","Was geht"};
+    std::cout << sizeof(words);
+}
+
+void changeThat(int &kek){
+    // Hier kann die Referenz angepasst werden und die Orginall Variable geändert werden
+    kek = 4;
 }
 
 int main()
 {
     // konsolenAusgabe();
     // rechner();
-    begruessen();
+    // begruessen();
+    // maxMin();
+    // quadrieren(3);
+    // std::cout << isMe();
+    // loops();
+
     return 0;
 }
